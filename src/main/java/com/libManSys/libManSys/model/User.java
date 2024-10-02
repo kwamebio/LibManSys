6805d5502501355fbaac2aa3ce796a1d69413358
kwamebio <kwameagyemang73@gmail.com>
2024-10-02 19:06:24 +0100

    ignore password field when fetching user data

--- src/main/java/com/libManSys/libManSys/model/User.java
+++ src/main/java/com/libManSys/libManSys/model/User.java
@@ -1,5 +1,7 @@
 package com.libManSys.libManSys.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +24,7 @@ public class User {
     @Column(name = "email")
     private String email;
 
+    @JsonIgnore
     @Column(name = "password")
     private String password;
 
